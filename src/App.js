3e10ce9795802c72e198ad621ddc14ae9c2db0a3
Willie Rose <willierose87@gmail.com>
2021-03-24 08:35:42 -0500

    Added stater files for memory game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Memory Game</h1>
+        <h3 className="subTitle">Match cards to win</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
